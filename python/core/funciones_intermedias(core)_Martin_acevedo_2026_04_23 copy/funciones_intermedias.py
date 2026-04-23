cbce14b99e97e55b5bb420ecc544df310a7025b7
kagekaoo942 <martinacevedo@liceovvh.cl>
2026-04-23 09:23:30 -0400

    terminar

--- python/core/funciones_intermedias(core)_Martin_acevedo_2026_04_23 copy/funciones_intermedias.py
+++ python/core/funciones_intermedias(core)_Martin_acevedo_2026_04_23 copy/funciones_intermedias.py
@@ -20,3 +20,65 @@ eventos = {
 ubicacion = [
    {"latitud": 34.052235, "longitud": -118.243683}
 ]
+
+#Parte 1:
+#1. En puntajes, cambia el valor 300 por 600 (ajuste en los puntajes de la primera ronda). Resultado esperado:
+
+'''
+2. En streamers, cambia el nombre de ”GameNinjaPro” a ”EliteGamerX”.
+En eventos, cambia la ciudad “Las Vegas” por “San Francisco”.
+En ubicacion, cambia el valo de ”latitud” a 40.712776 (cambiando la sede del torneo a Nueva York).
+'''
+
+
+#Parte 2
+'''
+Crea la función iterar_diccionario(lista) que reciba una lista de diccionarios (como streamers) y recorra cada uno, imprimiendo sus claves y valores.
+Formatea la salida para que cada diccionario se imprima en una sola línea, con el formato.
+'''
+#Obtener valores de un diccionario creando la función obtener_valores(clave, lista) que reciba, por una parte, una cadena con el nombre de una clave, por otra, una lista de diccionarios. La función debe imprimir el valor asociado a esa clave en cada uno de los diccionarios.
+#Valores esperados:
+#EliteGamerX
+#PixelWarrior
+
+#250000
+#180000
+
+#Parte 3:
+
+'''
+Crea la función mostrar_informacion(diccionario), que reciba un diccionario en el que los valores sean listas. La función debe, por una parte, imprimir el tamaño de la lista y la clave en mayúsculas, por otra, imprimir cada elemento de la lista en líneas separadas.
+Ejemplo de uso:
+'''
+
+'''
+categorias = {
+   "juegos_populares": [
+      "Fortnite", 
+      "Minecraft", 
+      "Valorant", 
+      "GTA V",
+   ],
+   "ciudades_eventos": [
+      "Nueva York",
+      "Madrid",
+      "Tokio",
+   ]
+}
+
+mostrar_informacion(categorias)​
+'''
+
+'''
+Salida esperada:
+4 JUEGOS_POPULARES
+Fortnite
+Minecraft
+Valorant
+GTA V
+
+3 CIUDADES_EVENTOS
+Nueva York
+Madrid
+Tokio​
+'''
