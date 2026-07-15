8d3e36ba6a8499d8bfb4f5d4c2929c222c0d6b20
kagekaoo942 <martinacevedo@liceovvh.cl>
2026-07-15 12:53:07 -0400

    terminar

---
+++ python/flask/fundamentos/06._motor_plantillas_jinja2/server.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def inicio():
+
+    return render_template("index.html",
+    nombre ="Martin", curso ="4C", 
+    ciudad ="Santiago", anio ="2026",
+    profesor =False, 
+      tecnologias=[
+          
+        "Python",
+
+        "Flask",
+
+        "HTML",
+
+        "CSS"
+
+    ])
+@app.route("/jugador")
+def jugador():
+
+    return render_template("jugador.html",
+    jugador="kagekaoo",
+
+    puntaje=3600,
+
+    lider=True
+)
+
+    
+if __name__ == "__main__":
+
+    app.run(debug=True)
